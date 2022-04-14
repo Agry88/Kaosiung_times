0ee014df50cf14dd9d458c989f4d760c06e85fd8
Agry88 <rgok307085@gmail.com>
2022-04-14 21:01:29 +0800

    navbar fix

--- src/index.js
+++ src/index.js
@@ -18,15 +18,15 @@ window.addEventListener('DOMContentLoaded', event => {
   // Navbar shrink function
   var navbarShrink = function () {
       const navbarCollapsible = document.body.querySelector('#mainNav');
-      // if (!navbarCollapsible) {
-      //     return;
-      // }
+      if (!navbarCollapsible) {
+          return;
+      }
       // if (window.scrollY === 0) {
       //     navbarCollapsible.classList.remove('navbar-shrink')
       // } else {
       //     navbarCollapsible.classList.add('navbar-shrink')
       // }
-      // navbarCollapsible.classList.add('navbar-shrink')
+      navbarCollapsible.classList.add('navbar-shrink')
 
 
   };
